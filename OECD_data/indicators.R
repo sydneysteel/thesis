2874f0890570206478dd6d71188d6911e852eafe
Sydney Steel <sydneysteel@Sydneys-MacBook-Air-4.local>
2018-12-10 23:27:52 -0500

    Adding female share in government data

--- OECD_data/indicators.R
+++ OECD_data/indicators.R
@@ -1,30 +1,35 @@
 library(tidyverse)
+library(scales)
+library(janitor)
+library(readxl)
 
-# Reading in the childcare and maternity leave data from the OECD
+# Reading in the maternity leave data from the UNDP
 
-paid_leave <- read_csv("Childcare and Maternity Leave - OECD.csv") %>% 
-  select(COU, Country, Indicator, Year, Value)
+paid_leave <- read_csv("Mandatory paid maternity leave (days).csv", skip = 1) %>% 
+  select(Country, `2010`, `2011`, `2012`, `2013`, `2014`, `2015`, `2016`, `2017`)
 
-# I only want countries in the EU and the US so I will filter out
-# the others and save that as an object that I will use for tidying
-# the other OECD and World Bank datasets
+# I only want countries in the EU and the US so I have added an excel that
+# has the 29 countries with their 2 and 3 digit codes. I changed the country names
+# for the UK and the US because the long version in the countries excel did not
+# match with the names in other data sets when I tried merging them
+
+countries <- read_excel("country_codes.xlsx") %>% 
+  mutate(name = str_replace_all(name, "United Kingdom of Great Britain and Northern Ireland", 
+                                "United Kingdom"),
+         name = str_replace_all(name, "United States of America", "United States"))
+
+# Filtering for the countries I want and gathering the year and value variables so 
+# that years are no longer separate columns. I also joined the paid leave data
+# with the countries data in order to add a country code column so that I can eventually 
+# merge it with the other spreads. I also had to convert the Year column into numeric 
+# so it would match the other spreads
 
 paid_leave <- paid_leave %>% 
-  filter(!COU %in% c("ISL", "NZL", "AUS", "LUX", "TUR", "JPN", "MEX", "KOR", 
-                     "CHL", "ISR", "CAN", "CRI", "BRA", "COL", "IDN", "RUS", "ZAF"))
-countries <- paid_leave %>% 
-  select(COU, Country) %>% 
-  unique()
-
-# For this data I am only interested in the weeks of paid leave guaranteed 
-# for each country so I will filter out the other indicators. I also filtered 
-# the date range to only include years after 2008. Then, I spread the data so it is tidier
-
-paid_leave <- paid_leave %>% 
-  filter(Indicator == "Length of paid maternity and parental leave available to mothers in weeks",
-         Year >= 2008) %>% 
-  spread(key = Indicator, value = Value) %>% 
-  rename(weeks_leave = "Length of paid maternity and parental leave available to mothers in weeks")
+  subset(Country %in% countries$name) %>% 
+  gather(key = Year, value = mandatory_leave_days, `2010`:`2017`) %>% 
+  left_join(countries, by = c("Country" = "name")) %>% 
+  mutate(Year = as.numeric(Year)) %>% 
+  select(-`alpha-2`)
 
 # Reading in the social spending data from the OECD
 
@@ -35,7 +40,7 @@ family_exp <- read_csv("Social Spending on Families - OECD.csv") %>%
 # Note: this data is only available for 2009 - 2013
 
 family_exp <- family_exp %>% 
-  subset(COU %in% countries$COU) %>% 
+  subset(COU %in% countries$`alpha-3`) %>% 
   filter(Year >= 2008,
          Indicator == "Total public social expenditure on families as a % of GDP") %>% 
   spread(key = Indicator, value = Value) %>% 
@@ -43,60 +48,61 @@ family_exp <- family_exp %>%
 
 # Reading in the female labor force participation data from the World Bank
 
-female_lfpr <- read_csv("Female LFPR - World Bank.csv") %>% 
-  select(-`Series Code`)
+female_lfpr <- read_excel("Female LFPR - World Bank.xls") %>% 
+  clean_names() %>% 
+  select(-series_code)
 
 # Filtering for my countries, and gathering the year and value variables so 
 # that years are no longer separate columns. Finally, I removed the characters
 # from the year column
 
 female_lfpr <- female_lfpr %>% 
-  subset(`Country Code` %in% countries$COU) %>% 
-  gather(key = Year, value = lfpr_female, `2008 [YR2008]`:`2017 [YR2017]`) %>% 
-  select(-`Series Name`) %>% 
+  subset(country_code %in% countries$`alpha-3`) %>% 
+  gather(key = Year, value = lfpr_female, x2008_yr2008:x2017_yr2017) %>% 
+  select(-series_name) %>% 
   mutate(Year = parse_number(Year))
 
 # Reading in service sector employment data from the World Bank
 
-services_employ <- read_csv("Service Sector Employment - World Bank.csv") %>% 
-  select(-`Series Code`)
+services_employ <- read_excel("Service Sector Employment - World Bank.xls") %>% 
+  clean_names() %>% 
+  select(-series_code)
 
 # Repeat steps as above
 
 services_employ <- services_employ %>% 
-  subset(`Country Code` %in% countries$COU) %>% 
-  gather(key = Year, value = employment_services, `2008 [YR2008]`:`2017 [YR2017]`) %>% 
-  select(-`Series Name`) %>% 
+  subset(country_code %in% countries$`alpha-3`) %>% 
+  gather(key = Year, value = employment_services, x2008_yr2008:x2017_yr2017) %>% 
+  select(-series_name) %>% 
   mutate(Year = parse_number(Year))
 
-# Reading in female tertiary enrollment rates from the World Bank
+# Reading in female tertiary enrollment rates from UNESCO
 
-tertiary_female <- read_csv("Female Tertiary Enrollment - World Bank.csv",
-                            na = c("..", "NA")) %>% 
-  select(-`Series Code`)
+tertiary_female <- read_excel("Tertiary Enrollment - UNESCO.xls") %>% 
+  select(-EDULIT_IND, -TIME, -Flags, -`Flag Codes`)
 
-# Repeat steps as above
+# Filtering for the countries and organizing variable names
 
 tertiary_female <- tertiary_female %>% 
-  subset(`Country Code` %in% countries$COU) %>% 
-  gather(key = Year, value = female_tertiary_enroll, `2008 [YR2008]`:`2017 [YR2017]`) %>% 
-  select(-`Series Name`) %>% 
-  mutate(Year = parse_number(Year))
+  subset(LOCATION %in% countries$`alpha-3`) %>% 
+  rename(COU = LOCATION, Year = Time, per_female_tertiary = Value) %>% 
+  select(-Indicator)
 
 # Reading in maternal leave indicator data from the World Bank
 
-leave_guarantee <- read_csv("Maternal Leave - World Bank.csv") %>% 
-  select(-`Series Code`)
+leave_guarantee <- read_excel("Maternal Leave - World Bank.xls") %>% 
+  clean_names() %>% 
+  select(-series_code)
 
 # Repeat steps as above
 
 leave_guarantee <- leave_guarantee %>% 
-  subset(`Country Code` %in% countries$COU) %>% 
-  gather(key = Year, value = position_guarantee, `2008 [YR2008]`:`2017 [YR2017]`) %>% 
-  select(-`Series Name`) %>% 
+  subset(country_code %in% countries$`alpha-3`) %>% 
+  gather(key = Year, value = position_guarantee, x2008_yr2008:x2017_yr2017) %>% 
+  select(-series_name) %>% 
   mutate(Year = parse_number(Year),
          position_guarantee = str_replace_all(position_guarantee, 
-                                              c("1" = "Yes", "0" = "No", ".." = "No leave available")))
+                                              c("1" = "Yes", "0" = "No")))
 
 # Reading in the OECD Employment Protection Legislation Index data
 
@@ -109,18 +115,48 @@ epl <- read_csv("EPL Index - OECD.csv") %>%
 # more data items relating to regulations on collective dismissals
 
 epl <- epl %>% 
-  subset(COUNTRY %in% countries$COU) %>% 
+  subset(COUNTRY %in% countries$`alpha-3`) %>% 
   filter(SERIES == "EPRC_V3") %>% 
   rename(epl_score = Value, Year = Time, COU = COUNTRY) %>% 
   select(-SERIES, - Series)
 
+# Reading in women's representation in government data from UNDP
+
+female_govt <- read_csv("Share of seats in parliament (% held by women).csv",
+                        skip = 1) %>% 
+  select(Country, `2010`, `2011`, `2012`, `2013`, `2014`, `2015`, `2016`, `2017`)
+
+# Filtering for the countries I want and gathering the year and value variables so 
+# that years are no longer separate columns. I also joined the female government data
+# with the countries data in order to add a country code column so that I can eventually 
+# merge it with the other spreads
+
+female_govt <- female_govt %>% 
+  subset(Country %in% countries$name) %>% 
+  gather(key = Year, value = female_politicians, `2010`:`2017`) %>% 
+  left_join(countries, by = c("Country" = "name")) %>% 
+  mutate(Year = as.numeric(Year)) %>% 
+  select(-`alpha-2`)
+
 # Joining the separate spreads
 
-indicators <- paid_leave %>% 
-  left_join(family_exp, by = c("COU", "Country", "Year")) %>% 
-  left_join(female_lfpr, by = c("Country" = "Country Name", "COU" = "Country Code", "Year")) %>% 
-  left_join(services_employ, by = c("Country" = "Country Name", "COU" = "Country Code", "Year")) %>% 
-  left_join(tertiary_female, by = c("Country" = "Country Name", "COU" = "Country Code", "Year")) %>% 
-  left_join(leave_guarantee, by = c("Country" = "Country Name", "COU" = "Country Code", "Year")) %>% 
-  left_join(epl, by = c("COU", "Country", "Year"))
+indicators <- female_lfpr %>% 
+  left_join(family_exp, by = c("country_code" = "COU", "Year")) %>% 
+  left_join(tertiary_female, by = c("country_code" = "COU", "Year")) %>% 
+  left_join(leave_guarantee, by = c("country_code", "Year")) %>% 
+  left_join(paid_leave, by = c("country_code" = "alpha-3", "Year")) %>% 
+  left_join(services_employ, by = c("country_code", "Year")) %>% 
+  left_join(epl, by = c("country_code" = "COU", "Year")) %>% 
+  left_join(female_govt, by = c("country_code" = "alpha-3", "Year")) %>% 
+  select(Year, country_code, country_name, everything(), -country_name.x, -Country.x, -Country.y,
+         -country_name.y, -Country.x.x, -Country.y.y, -Country)
+
+# Create file and send to main folder
+
+directory <- "/Users/sydneysteel/Thesis"
+
+write.csv(indicators, file = file.path(directory, "indicators_data.csv"))
+
+write.csv(countries, file = file.path(directory, "country_codes.csv"))
+
 
